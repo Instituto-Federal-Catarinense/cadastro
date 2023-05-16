facdc88f722ce5291352881fb06cc9e75d965b33
vitoria pires <vitoria.pires23.03@gmail.com>
2023-05-16 11:28:37 -0300

    aula 16 05

--- consulta.js
+++ consulta.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
-  password: 'A1b1c1d1',
+  password: 'aluno01',
   database: 'meuBanco'
 });
 
@@ -30,8 +30,16 @@ app.get('/consulta', (req, res) => {
         <form method="POST" action="/clientes">
           <label for="nome">Nome:</label>
           <input type="text" id="nome" name="nome"><br><br>
+
           <label for="endereco">Endereço:</label>
           <input type="text" id="endereco" name="endereco"><br><br>
+
+          <label for="sexo">sexo:</label>
+          <input type="text" id="sexo" name="sexo"><br><br>
+
+          <label for="idade">idade:</label>
+          <input type="text" id="idade" name="endereco"><br><br>
+
           <button type="submit">Consultar</button>
         </form>
       </body>
