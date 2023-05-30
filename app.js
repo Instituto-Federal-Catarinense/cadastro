01a8ed493b8a30cb5fcadc8673cccfea6d1c0028
João Francisco <jfranciscobratti@gmai.com>
2023-05-30 10:11:52 -0300

    Produtos

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ const connection = mysql.createConnection({
   database: "meuBanco"
 });
 
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+
+// Clientes:
 
 app.get("/cadastro", (req, res) => {
   res.sendFile(__dirname + "/cadastro.html");
@@ -92,7 +96,7 @@ app.get('/consulta', (req, res) => {
       </head>
       <body>
         <h1>Consulta de clientes</h1>
-        <form method="POST" action="/consulta">
+        <form method="POST" action="/consultaClientes">
           <label for="nome">Nome:</label>
           <input type="text" id="nome" name="nome"><br><br>
           <button type="submit">Consultar</button>
@@ -152,6 +156,142 @@ app.post('/consulta', (req, res) => {
   });
 });
 
+
+// Produtos:
+
+
+app.get("/cadProdutos", (req, res) => {
+  res.sendFile(__dirname + "/cadProdutos.html");
+});
+
+app.post("/cadProdutos", (req, res) => {
+  const { descricao , quantidade , valor } = req.body;
+  if (!descricao || !quantidade || !valor) {
+    res.status(400).send("Descrição, Quantidade e valor são campos obrigatórios.");
+    return;
+  }
+
+  const produtos = { descricao, quantidade , valor };
+  connection.query("INSERT INTO produtos SET ?", produtos, (err, result) => {
+    if (err) throw err;
+    console.log(`Produtos ${descricao} cadastrado com sucesso!`);
+    res.redirect("/");
+  });
+});
+
+// Rota para processar a listagem
+app.get('/listagemProduto', (req, res) => {
+
+  // Consulta no banco de dados
+  connection.query(`SELECT * FROM produtos`, (error, results, fields) => {
+    if (error) throw error;
+    
+    // Exibição dos resultados
+    let html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Produtos</title>
+        </head>
+        <body>
+          <h1>Produtos encontrados</h1>
+          <table>
+            <tr>
+              <th>Descrição</th>
+              <th>quantidade</th>
+              <th>valor</th>
+            </tr>
+    `;
+    
+    results.forEach((produtos) => {
+      html += `
+        <tr>
+          <td>${produtos.descricao}</td>
+          <td>${produtos.quantidade}</td>
+          <td>${produtos.valor}</td>
+        </tr>
+      `;
+    });
+    
+    html += `
+          </table>
+          <a href="/">Voltar</a>
+        </body>
+      </html>
+    `;
+    
+    res.send(html);
+  });
+});
+
+// Rota para exibir o formulário de consulta
+app.get('/consultaProduto', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Consulta de Produtos</title>
+      </head>
+      <body>
+        <h1>Consulta de Produtos</h1>
+        <form method="POST" action="/consultaProduto">
+          <label for="descricao">Descrição:</label>
+          <input type="text" id="descricao" name="descricao"><br><br>
+          <button type="submit">Consultar</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+// Rota para processar a consulta
+app.post('/consultaProduto', (req, res) => {
+  //const descricao = req.body.nome;
+  const { descricao, quantidade, valor } = req.body;
+  //const quantidade = req.body.quantidade;
+  
+  // Consulta no banco de dados
+  connection.query(`SELECT * FROM produtos WHERE descricao LIKE '%${descricao}%'`, (error, results, fields) => {
+    if (error) throw error;
+    
+    // Exibição dos resultados
+    let html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Produtos</title>
+        </head>
+        <body>
+          <h1>Produtos encontrados</h1>
+          <table>
+            <tr>
+              <th>Descrição</th>
+              <th>Quantidade</th>
+              <th>Valor</th>
+            </tr>
+    `;
+    
+    results.forEach((produto) => {
+      html += `
+        <tr>
+          <td>${produto.descricao}</td>
+          <td>${produto.quantidade}</td>
+          <td>${produto.valor}</td>
+        </tr>
+      `;
+    });
+    
+    html += `
+          </table>
+          <a href="/">Voltar</a>
+        </body>
+      </html>
+    `;
+    
+    res.send(html);
+  });
+});
+
 connection.connect((err) => {
   if (err) throw err;
   console.log("Conectado ao banco de dados MySQL!");
@@ -160,4 +300,3 @@ connection.connect((err) => {
 app.listen(3000, () => {
   console.log("Servidor iniciado na porta 3000");
 });
-ndereco
