26ac222f25bd6ed07c2b4164c8704d1ec20f7917
Maria Eduarda <dudinhaps04@hotmail.com>
2023-06-02 15:51:12 -0300

    versão cadastro de produtos

--- app.js
+++ app.js
@@ -8,18 +8,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "A1b1c1d1",
+  password: "aluno01",
   database: "meuBanco"
 });
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
-});
-
-app.get('/:nome', (req, res) => {
-  const userNome = req.params.nome;
-  // faça algo com o userId
-  console.log(`O nome do usuário é ${userNome}`);
 });
 
 app.get("/cadastro", (req, res) => {
@@ -135,6 +129,145 @@ app.post('/consulta', (req, res) => {
         <tr>
           <td>${cliente.nome}</td>
           <td>${cliente.endereco}</td>
+        </tr>
+      `;
+    });
+    
+    html += `
+          </table>
+          <a href="/">Voltar</a>
+        </body>
+      </html>
+    `;
+    
+    res.send(html);
+  });
+});
+
+// cadastro de produtos
+app.get("/cadastrarProdutos", (req, res) => {
+  res.sendFile(__dirname + "/cadastroProdutos.html");
+});
+
+app.post("/cadastrarProdutos", (req, res) => {
+  const { descricao, quantidade, valor } = req.body;
+  if (!descricao || !quantidade || !valor) {
+    res.status(400).send("descrição, quantidade e valor são campos obrigatórios.");
+    return;
+  }
+
+  const produto = { descricao, quantidade, valor };
+  connection.query("INSERT INTO produtos SET ?", produto, (err, result) => {
+    if (err) throw err;
+    console.log(`produto ${descricao} cadastrado com sucesso!`);
+    res.redirect("/");
+  });
+});
+
+// Rota para processar a listagem
+app.get('/listarprodutos', (req, res) => {
+
+  // Consulta no banco de dados
+  connection.query(`SELECT * FROM produtos`, (error, results, fields) => {
+    if (error) throw error;
+    
+    // Exibição dos resultados
+    let html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>produtos</title>
+        </head>
+        <body>
+          <h1>produtos encontrados</h1>
+          <table>
+            <tr>
+              <th>descrição</th>
+              <th>quantidade</th>
+              <th>valor</th>
+            </tr>
+    `;
+    
+    results.forEach((produtos) => {
+      html += `
+        <tr>
+          <td>${produtos.id}</td>
+          <td>${produtos.descricao}</td>
+          <td>${produtos.quantidade}</td>
+          <td>${produtos.valor}</td>
+        </tr>
+      `;
+    });
+    
+    html += `
+          </table>
+          <a href="/">Voltar</a>
+        </body>
+      </html>
+    `;
+    
+    res.send(html);
+  });
+});
+
+// Rota para exibir o formulário de consulta
+app.get('/consultarprodutos', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Consulta de Produtos</title>
+      </head>
+      <body>
+        <h1>Consulta de Produtos</h1>
+        <form method="POST" action="/consultarprodutos">
+          <label for="descricao">Descrição:</label>
+          <input type="text" id="descricao" name="descricao">
+          <br>
+          <label for="quantidade">Quantidade:</label>
+          <input type="text" id="quantidade" name="quantidade">
+          <br>
+          <label for="valor">Valor:</label>
+          <input type="text" id="valor" name="valor">
+          <br>
+          <button type="submit">Consultar</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+// Rota para processar a consulta
+app.post('/consultarprodutos', (req, res) => {
+  //const nome = req.body.nome;
+  const { descricao } = req.body;
+  //const endereco = req.body.endereco;
+  
+  // Consulta no banco de dados
+  connection.query(`SELECT * FROM produtos WHERE nome LIKE '%${descricao}%'`, (error, results, fields) => {
+    if (error) throw error;
+    
+    // Exibição dos resultados
+    let html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>produtos</title>
+        </head>
+        <body>
+          <h1>Produtos encontrados</h1>
+          <table>
+            <tr>
+            <th>descrição</th>
+            </tr>
+    `;
+    
+    results.forEach((produtos) => {
+      html += `
+        <tr>
+          <td>${produtos.descricao}</td>
+          <td>${produtos.quantidade}</td>
+          <td>${produtos.valor}</td>
         </tr>
       `;
     });
