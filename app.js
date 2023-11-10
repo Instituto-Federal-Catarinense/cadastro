97e135be4c70e2ab38bf0ebc9f68f08b1788b67d
heloisamtx <lolomachadoteixeira@gmail.com>
2023-11-10 09:02:25 -0300

    n9ova versao 10/11

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "A1b1c1d1",
+  password: "aluno01",
   database: "meuBanco"
 });
 
@@ -16,11 +16,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/:nome', (req, res) => {
-  const userNome = req.params.nome;
-  // faça algo com o userId
-  console.log(`O nome do usuário é ${userNome}`);
-});
+
 
 app.get("/cadastro", (req, res) => {
   res.sendFile(__dirname + "/cadastro.html");
