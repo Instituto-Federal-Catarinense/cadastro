f55f25eee1f8a825f7913c56c77f8925cadd0662
henrique <mamae6881@gmail.com>
2023-11-16 09:31:56 -0300

    banco

--- app.js
+++ app.js
@@ -16,24 +16,18 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/:nome', (req, res) => {
-  const userNome = req.params.nome;
-  // faça algo com o userId
-  console.log(`O nome do usuário é ${userNome}`);
-});
-
 app.get("/cadastro", (req, res) => {
   res.sendFile(__dirname + "/cadastro.html");
 });
 
 app.post("/cadastro", (req, res) => {
-  const { nome, endereco } = req.body;
-  if (!nome || !endereco) {
-    res.status(400).send("Nome e endereço são campos obrigatórios.");
+  const { nome, endereco, email, telefone, sexo, nascimento } = req.body;
+  if (!nome || !endereco|| !email|| !telefone|| !sexo|| !nascimento) {
+    res.status(400).send("todos sao obrigatorios.");
     return;
   }
 
-  const cliente = { nome, endereco };
+  const cliente = { nome, endereco, email, telefone, sexo, nascimento };
   connection.query("INSERT INTO clientes SET ?", cliente, (err, result) => {
     if (err) throw err;
     console.log(`Cliente ${nome} cadastrado com sucesso!`);
@@ -155,6 +149,6 @@ connection.connect((err) => {
   console.log("Conectado ao banco de dados MySQL!");
 });
 
-app.listen(3000, () => {
+app.listen(8080, () => {
   console.log("Servidor iniciado na porta 3000");
 });
