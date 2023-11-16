d8dd1a1419a9c985c035407b77c119d4549081e0
Eduardo Ferraz Alves <dudu.ferraz.alves@gmail.com>
2023-11-16 09:28:31 -0300

    primeira vesao

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "A1b1c1d1",
+  password: "aluno01",
   database: "meuBanco"
 });
 
@@ -16,24 +16,18 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/:nome', (req, res) => {
-  const userNome = req.params.nome;
-  // faça algo com o userId
-  console.log(`O nome do usuário é ${userNome}`);
-});
-
 app.get("/cadastro", (req, res) => {
   res.sendFile(__dirname + "/cadastro.html");
 });
 
 app.post("/cadastro", (req, res) => {
-  const { nome, endereco } = req.body;
-  if (!nome || !endereco) {
-    res.status(400).send("Nome e endereço são campos obrigatórios.");
+  const { nome, endereco, telefone, email, sexo, data_nasc} = req.body;
+  if (!nome || !endereco || !telefone || !email || !sexo || !data_nasc) {
+    res.status(400).send("todos os campos são obrigatórios.");
     return;
   }
 
-  const cliente = { nome, endereco };
+  const cliente = { nome, endereco, telefone, email, sexo, data_nasc };
   connection.query("INSERT INTO clientes SET ?", cliente, (err, result) => {
     if (err) throw err;
     console.log(`Cliente ${nome} cadastrado com sucesso!`);
@@ -61,6 +55,10 @@ app.get('/listagem', (req, res) => {
             <tr>
               <th>Nome</th>
               <th>endereco</th>
+              <th>telefone</th>
+              <th>email</th>
+              <th>sexo</th>
+              <th>data_nasc</th>
             </tr>
     `;
     
@@ -127,6 +125,10 @@ app.post('/consulta', (req, res) => {
             <tr>
               <th>Nome</th>
               <th>endereco</th>
+              <th>telefone</th>
+              <th>email</th>
+              <th>sexo</th>
+              <th>data_nasc</th>
             </tr>
     `;
     
