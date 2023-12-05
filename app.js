ec8390d3494675b7106e3cfe8cd8e34e8c7e1127
benadoBND <bernardonegriniborba@gmail.com>
2023-12-05 11:23:56 -0300

    Finalizado

--- app.js
+++ app.js
@@ -16,20 +16,18 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-
-
 app.get("/clientes", (req, res) => {
   res.sendFile(__dirname + "/clientes.html");
 });
 
 app.post("/clientes", (req, res) => {
-  const { nome, endereco, sexo, idade, nascimento, email, telefone } = req.body;
-  if (!nome || !endereco || !sexo || !idade || !nascimento || !email || !telefone) {
-    res.status(400).send("Nome e endereço são campos obrigatórios.");
+  const { nome, endereco, email, telefone, sexo, nascimento } = req.body;
+  if (!nome || !endereco || !email || !telefone || !sexo || !nascimento ) {
+    res.status(400).send("todos são campos obrigatórios.");
     return;
   }
 
-  const cliente = {  nome, endereco, sexo, idade, nascimento, email, telefone };
+  const cliente = { nome, endereco };
   connection.query("INSERT INTO clientes SET ?", cliente, (err, result) => {
     if (err) throw err;
     console.log(`Cliente ${nome} cadastrado com sucesso!`);
@@ -37,13 +35,32 @@ app.post("/clientes", (req, res) => {
   });
 });
 
+app.get("/produtos", (req, res) => {
+  res.sendFile(__dirname + "/produtos.html");
+});
+
+app.post("/produtos", (req, res) => {
+  const { nome, descricao, quantidade, valor } = req.body;
+  if (!nome || !descricao || !quantidade || !valor) {
+    res.status(400).send("todos são campos obrigatórios.");
+    return;
+  }
+
+  const produto = { nome, descricao, quantidade, valor };
+  connection.query("INSERT INTO produtos SET ?", produto, (err, result) => {
+    if (err) throw err;
+    console.log(`Produto ${nome} cadastrado com sucesso!`);
+    res.redirect("/");
+  });
+});
+
 // Rota para processar a listagem
-app.get('/listagem', (req, res) => {
+app.get('/listagem-clientes', (req, res) => {
 
   // Consulta no banco de dados
   connection.query(`SELECT * FROM clientes`, (error, results, fields) => {
     if (error) throw error;
-
+    
     // Exibição dos resultados
     let html = `
       <!DOCTYPE html>
@@ -56,36 +73,82 @@ app.get('/listagem', (req, res) => {
           <table>
             <tr>
               <th>Nome</th>
-              <th>Endereco</th>
-              <th>Sexo</th>
-              <th>Idade</th>
-              <th>Nascimento</th>
-              <th>Email</th>
-              <th>Telefone</th>
+              <th>endereco</th>
+              <th>email</th>
+              <th>telefone</th>
+              <th>sexo</th>
+              <th>nascimento</th>
             </tr>
     `;
-
+    
     results.forEach((cliente) => {
       html += `
         <tr>
           <td>${cliente.nome}</td>
           <td>${cliente.endereco}</td>
-          <td>${cliente.sexo}</td>
-          <td>${cliente.idade}</td>
-          <td>${cliente.nascimento}</td>
           <td>${cliente.email}</td>
           <td>${cliente.telefone}</td>
+          <td>${cliente.sexo}</td>
+          <td>${cliente.nascimento}</td>
         </tr>
       `;
     });
-
+    
     html += `
           </table>
           <a href="/">Voltar</a>
         </body>
       </html>
     `;
+    
+    res.send(html);
+  });
+});
 
+app.get('/listagem-produtos', (req, res) => {
+
+  // Consulta no banco de dados
+  connection.query(`SELECT * FROM produtos`, (error, results, fields) => {
+    if (error) throw error;
+    
+    // Exibição dos resultados
+    let html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Clientes</title>
+        </head>
+        <body>
+          <h1>Clientes encontrados</h1>
+          <table>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Descrição</th>
+              <th>Quantidade</th>
+              <th>Valor</th>
+            </tr>
+    `;
+    
+    results.forEach((produto) => {
+      html += `
+        <tr>
+          <td>${produto.id}</td>
+          <td>${produto.nome}</td>
+          <td>${produto.descricao}</td>
+          <td>${produto.quantidade}</td>
+          <td>R$ ${produto.valor}</td>
+        </tr>
+      `;
+    });
+    
+    html += `
+          </table>
+          <a href="/">Voltar</a>
+        </body>
+      </html>
+    `;
+    
     res.send(html);
   });
 });
@@ -113,13 +176,13 @@ app.get('/consulta', (req, res) => {
 // Rota para processar a consulta
 app.post('/consulta', (req, res) => {
   //const nome = req.body.nome;
-  const { nome, endereco, sexo, idade, nascimento, email, telefone  } = req.body;
+  const { nome, endereco } = req.body;
   //const endereco = req.body.endereco;
-
+  
   // Consulta no banco de dados
   connection.query(`SELECT * FROM clientes WHERE nome LIKE '%${nome}%'`, (error, results, fields) => {
     if (error) throw error;
-
+    
     // Exibição dos resultados
     let html = `
       <!DOCTYPE html>
@@ -130,38 +193,37 @@ app.post('/consulta', (req, res) => {
         <body>
           <h1>Clientes encontrados</h1>
           <table>
-          <tr>
-            <th>Nome</th>
-            <th>Endereco</th>
-            <th>Sexo</th>
-            <th>Idade</th>
-            <th>Nascimento</th>
-            <th>Email</th>
-            <th>Telefone</th>
-          </tr>
-    `;  
-
+            <tr>
+              <th>Nome</th>
+              <th>endereco</th>
+              <th>email</th>
+              <th>telefone</th>
+              <th>sexo</th>
+              <th>nascimento</th>
+            </tr>
+    `;
+    
     results.forEach((cliente) => {
       html += `
-      <tr>
-        <td>${cliente.nome}</td>
-        <td>${cliente.endereco}</td>
-        <td>${cliente.sexo}</td>
-        <td>${cliente.idade}</td>
-        <td>${cliente.nascimento}</td>
-        <td>${cliente.email}</td>
-        <td>${cliente.telefone}</td>
-      </tr>
+        <tr>
+          <td>${cliente.nome}</td>
+          <td>${cliente.endereco}</td>
+          <td>${cliente.email}</td>
+          <td>${cliente.telefone}</td>
+          <td>${cliente.nascimento}</td>
+          <td>${cliente.sexo}</td>
+        </tr>
       `;
     });
-
+    
     html += `
           </table>
+          <br>
           <a href="/">Voltar</a>
         </body>
       </html>
     `;
-
+    
     res.send(html);
   });
 });
@@ -171,6 +233,6 @@ connection.connect((err) => {
   console.log("Conectado ao banco de dados MySQL!");
 });
 
-app.listen(5000, () => {
-  console.log("Servidor iniciado na porta 5000");
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta 3000");
 });
--- meuBanco.sql
+++ meuBanco.sql
@@ -1,13 +1,15 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+CREATE DATABASE  IF NOT EXISTS `meubanco` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `meubanco`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: localhost    Database: meuBanco
+-- Host: localhost    Database: meubanco
 -- ------------------------------------------------------
--- Server version	8.0.32-0ubuntu0.22.04.2
+-- Server version	8.0.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -18,16 +20,20 @@
 --
 -- Table structure for table `clientes`
 --
-CREATE DATABASE meuBanco;
-USE meuBanco;
 
 DROP TABLE IF EXISTS `clientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) DEFAULT NULL,
-  `endereco` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `endereco` varchar(255) DEFAULT NULL,
+  `email` varchar(25) DEFAULT NULL,
+  `telefone` varchar(30) DEFAULT NULL,
+  `sexo` varchar(20) DEFAULT NULL,
+  `nascimento` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,8 +42,35 @@ CREATE TABLE `clientes` (
 
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
-INSERT INTO `clientes` VALUES ('dfgdfg','dfgsdfg'),('Joedio','Arroio do Silva'),('Teste','teste'),('aloou','alou'),('teste2','teste2'),('Teste4','Teste4'),('teste5','teste5'),('teste6','teste6');
+INSERT INTO `clientes` VALUES (1,'Samuel Conradt Amaral','Arroio sla doq, 211, sal, brasil','conradtsamuel@gmail.com','129009829332','masculino','2006-11-16');
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produtos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `descricao` text,
+  `quantidade` bigint DEFAULT NULL,
+  `valor` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+INSERT INTO `produtos` VALUES (1,'Batatas','Muitas batatas',12222222222,20.5);
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -49,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-04 11:48:48
+-- Dump completed on 2023-12-04 17:58:59
